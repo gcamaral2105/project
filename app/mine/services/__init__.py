259d8bf1373777f5d8056805bdddcf0c264e6b39
gcamaral2105 <gabrieldecarvalho2105@gmail.com>
2025-08-21 10:36:37 -0300

    Create __init__.py

---
+++ app/mine/services/__init__.py
@@ -0,0 +1,10 @@
+"""
+Mine Services Module
+===================
+
+This module contains business logic services for mine management.
+"""
+
+from .mine_service import MineService
+
+__all__ = ["MineService"]
