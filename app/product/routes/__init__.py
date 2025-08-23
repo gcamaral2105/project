2f77de89cc164639ff1d20bb59c2ef7a96fdd736
gcamaral2105 <gabrieldecarvalho2105@gmail.com>
2025-08-22 21:51:30 -0300

    Update __init__.py

--- app/product/routes/__init__.py
+++ app/product/routes/__init__.py
@@ -1,10 +1,1 @@
-"""
-Product Routes Module
-====================
 
-This module contains Flask routes for product management endpoints.
-"""
-
-from .product_routes import product_bp
-
-__all__ = ["product_bp"]
