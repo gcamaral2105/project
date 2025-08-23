131d1f6c3f5b762c3fd0e2c370452c2d79a54618
gcamaral2105 <gabrieldecarvalho2105@gmail.com>
2025-08-22 21:14:48 -0300

    Update __init__.py

---
+++ app/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from .extensions import db, migrate
+
+def create_app(config_class='config.Config'):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    with app.app_context():
+
+    return app
